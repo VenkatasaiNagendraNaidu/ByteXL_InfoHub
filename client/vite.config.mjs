4369ddb55ccc50324347328e37c11632d72c5163
VICKY SKVH <venkatasainagendranaidualthi@gmail.com>
2025-11-06 08:01:53 +0530

    Completed

--- client/vite.config.mjs
+++ client/vite.config.mjs
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  // critical: do NOT load PostCSS config; we use CLI CSS
   css: { postcss: {} }
 });
